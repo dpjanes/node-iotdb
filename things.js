034548ce9eb6896f02ed1b38eef5730bd90c6f3b
David Janes <davidjanes@davidjanes.com>
2015-06-27 17:01:34 -0400

    adding wrong things to thinbgs

--- things.js
+++ things.js
@@ -68,10 +68,7 @@ Things.prototype.things = function (model_code) {
     });
 
     var _add = function (thing) {
-        if (!thing.reachable()) {
-            // not sure if this is a good idea
-            things.push(thing);
-        } else if (!model_code) {
+        if (!model_code) {
             things.push(thing);
         } else if (model_code === thing.code()) {
             things.push(thing);
