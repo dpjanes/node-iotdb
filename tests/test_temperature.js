1ad5d16f891c7939bed56dc5267b85fa1b21102f
David Janes <davidjanes@davidjanes.com>
2015-12-31 08:17:16 -0330

    temperature test cases

---
+++ tests/test_temperature.js
@@ -0,0 +1,70 @@
+/*
+ *  test_d_temperature.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2015-12-31
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+
+/* --- tests --- */
+describe('test_temperature', function(){
+    describe('c->f', function(){
+        it('-40', function() {
+            var c = -40;
+            var f = -40;
+            var r = _.temperature.c2f(c);
+            assert.strictEqual(f, r);
+        });
+        it('0', function() {
+            var c = 0;
+            var f = 32;
+            var r = _.temperature.c2f(c);
+            assert.strictEqual(f, r);
+        });
+        it('20', function() {
+            var c = 20;
+            var f = 68;
+            var r = _.temperature.c2f(c);
+            assert.strictEqual(f, r);
+        });
+        it('100', function() {
+            var c = 100;
+            var f = 212;
+            var r = _.temperature.c2f(c);
+            assert.strictEqual(f, r);
+        });
+    });
+    describe('f->c', function(){
+        it('-40', function() {
+            var c = -40;
+            var f = -40;
+            var r = _.temperature.f2c(f);
+            assert.strictEqual(c, r);
+        });
+        it('32', function() {
+            var c = 0;
+            var f = 32;
+            var r = _.temperature.f2c(f);
+            assert.strictEqual(c, r);
+        });
+        it('68', function() {
+            var c = 20;
+            var f = 68;
+            var r = _.temperature.f2c(f);
+            assert.strictEqual(c, r);
+        });
+        it('212', function() {
+            var c = 100;
+            var f = 212;
+            var r = _.temperature.f2c(f);
+            assert.strictEqual(c, r);
+        });
+    });
+})
