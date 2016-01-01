282e43dc04d6189b6275f2ea96f37caa1a2fd007
David Janes <davidjanes@davidjanes.com>
2016-01-01 11:09:53 -0500

    more testing stubs

---
+++ tests/test_error.js
@@ -0,0 +1,27 @@
+/*
+ *  test_error.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-01
+ *  "The Frickin Future"
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+describe('test_error', function() {
+    describe('message', function() {
+        it('empty', function() {
+        });
+        it('string', function() {
+        });
+        it('error', function() {
+        });
+        it('bad', function() {
+        });
+    });
+});
---
+++ tests/test_hash.js
@@ -0,0 +1,49 @@
+/*
+ *  test_hash.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-01
+ *  "The Frickin Future"
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+describe('test_hash', function() {
+    describe('md5', function() {
+        it('empty', function() {
+        });
+        it('string', function() {
+        });
+        it('complex', function() {
+        });
+    });
+    describe('sha1', function() {
+        it('empty', function() {
+        });
+        it('string', function() {
+        });
+        it('complex', function() {
+        });
+    });
+    describe('sha256', function() {
+        it('empty', function() {
+        });
+        it('string', function() {
+        });
+        it('complex', function() {
+        });
+    });
+    describe('sha512', function() {
+        it('empty', function() {
+        });
+        it('string', function() {
+        });
+        it('complex', function() {
+        });
+    });
+});
