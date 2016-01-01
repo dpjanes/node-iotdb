295614b7d27eb0baf596bf02c33b4fee718c88a8
David Janes <davidjanes@davidjanes.com>
2016-01-01 11:04:33 -0500

    stub for testing helpers/net

---
+++ tests/test_net.js
@@ -0,0 +1,29 @@
+/*
+ *  test_net.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-01
+ *  "The Frickin Future"
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+describe('test_net', function() {
+    describe('ipv4', function() {
+        it('default', function() {
+        });
+    });
+    describe('ipv6', function() {
+        it('default', function() {
+        });
+    });
+    describe('mac', function() {
+        it('default', function() {
+        });
+    });
+});
