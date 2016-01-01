911b494b9c9b2baf1443d101ebdc4da638514c03
David Janes <davidjanes@davidjanes.com>
2016-01-01 11:03:23 -0500

    stub for testing unit conversion

---
+++ tests/test_convert.js
@@ -0,0 +1,55 @@
+/*
+ *  test_convert.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-01
+ *  "The Frickin Future"
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+describe('test_convert', function() {
+    describe('add', function() {
+        it('default', function() {
+        });
+    });
+    describe('convert', function() {
+        describe('simple', function() {
+            describe('c->f', function() {
+                it('default', function() {
+                });
+            });
+            describe('f->c', function() {
+                it('default', function() {
+                });
+            });
+            describe('c->k', function() {
+                it('default', function() {
+                });
+            });
+            describe('percent->unit', function() {
+                it('default', function() {
+                });
+            });
+            describe('unit->percent', function() {
+                it('default', function() {
+                });
+            });
+        });
+        describe('deep', function() {
+            describe('f->k', function() {
+                it('default', function() {
+                });
+            });
+            describe('k->f', function() {
+                it('default', function() {
+                });
+            });
+        });
+    });
+});
