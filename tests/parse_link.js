6f339724dd7dcf443eb32d01e75e3a006b256714
David Janes <davidjanes@davidjanes.com>
2016-01-01 11:06:34 -0500

    stub for testing helpers/parse_link

---
+++ tests/parse_link.js
@@ -0,0 +1,29 @@
+/*
+ *  test_parse_link.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-01
+ *  "The Frickin Future"
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+describe('test_parse_link', function() {
+    describe('parse_link', function() {
+        it('empty', function() {
+        });
+        it('single', function() {
+        });
+        it('multi', function() {
+        });
+        it('multi-line', function() {
+        });
+        it('bad', function() {
+        });
+    });
+});
