6e29b535f35eaa610672a3316b81e049b8a68257
David Janes <davidjanes@davidjanes.com>
2016-02-03 06:49:34 -0500

    more tests

--- tests/test_error.js
+++ tests/test_error.js
@@ -44,4 +44,57 @@ describe('test_error', function() {
             assert.strictEqual(result, expect);
         });
     });
+    describe('code', function() {
+        it('empty', function() {
+            var value = undefined;
+            var result = _.error.code(value);
+            var expect = 500;
+
+            assert.strictEqual(result, expect);
+        });
+        it('string', function() {
+            var value = "an error";
+            var result = _.error.code(value);
+            var expect = 500;
+
+            assert.strictEqual(result, expect);
+        });
+        it('string with otherwise', function() {
+            var value = "an error";
+            var result = _.error.code(value, 404);
+            var expect = 404;
+
+            assert.strictEqual(result, expect);
+        });
+        it('error', function() {
+            var value = new Error("an error");
+            var result = _.error.code(value);
+            var expect = 500;
+
+            assert.strictEqual(result, expect);
+        });
+        it('error - with code', function() {
+            var value = new Error("an error");
+            value.code = 403;
+            var result = _.error.code(value);
+            var expect = 403;
+
+            assert.strictEqual(result, expect);
+        });
+        it('error - with code and otherwise', function() {
+            var value = new Error("an error");
+            value.code = 403;
+            var result = _.error.code(value, 409);
+            var expect = 403;
+
+            assert.strictEqual(result, expect);
+        });
+        it('bad with otherwise', function() {
+            var value = 1234;
+            var result = _.error.code(value, 404);
+            var expect = 404;
+
+            assert.strictEqual(result, expect);
+        });
+    });
 });
