6f58eef06c99378e111cbd5cd3f40bd7d8eb9cee
David Janes <davidjanes@davidjanes.com>
2016-02-08 09:37:52 -0500

    new tests

---
+++ tests/test_thing_array_with_tag.js
@@ -0,0 +1,77 @@
+/*
+ *  test_thing_array_with_tag.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2016-01-06
+ */
+
+"use strict";
+
+var assert = require("assert")
+var sleep = require("sleep");
+var _ = require("../helpers")
+
+var iotdb = require("../iotdb");
+var things = require("../things");
+
+require('./instrument/iotdb');
+
+var _make_thing = function(callback) {
+    var t = new things.Things();
+    t._reset();
+    
+    var ts = t.connect("Test", {}, {
+        "schema:name": "The Thing Name",
+        "schema:description": "My Thing",
+        "iot:zone": [ "Glasgow Place", "Second Floor", "Bedroom" ],
+        "iot:facet": [ "iot-facet:switch", "iot-facet:lighting", "iot-facet:something" ],
+        "iot:thing-number": 32,
+    });
+    ts.tag([ "a", "b", "c" ]);
+    ts.on("thing", function() {
+        callback(ts);
+    });
+};
+
+describe('test_thing_array', function() {
+    describe('with', function() {
+        describe('with_tag', function() {
+            it('matching', function() {
+                _make_thing(function(ts) {
+                    var ms = ts.with_tag("a");
+
+                    assert.strictEqual(ms.length, 1);
+                });
+            });
+            it('matching with array', function() {
+                _make_thing(function(ts) {
+                    var ms = ts.with_tag([ "a", "b", "c"]);
+
+                    assert.strictEqual(ms.length, 1);
+                });
+            });
+            it('matching with array with some non matching items', function() {
+                _make_thing(function(ts) {
+                    var ms = ts.with_tag([ "c", "d", "e"]);
+
+                    assert.strictEqual(ms.length, 1);
+                });
+            });
+            it('not matching', function() {
+                _make_thing(function(ts) {
+                    var ms = ts.with_tag("e");
+
+                    assert.strictEqual(ms.length, 0);
+                });
+            });
+            it('not matching with array', function() {
+                _make_thing(function(ts) {
+                    var ms = ts.with_tag([ "e", "f", "g" ]);
+
+                    assert.strictEqual(ms.length, 0);
+                });
+            });
+        });
+    });
+});
