7fd2475d3342497737202ab3416b47ecc7f03978
David Janes <davidjanes@davidjanes.com>
2016-07-19 07:32:12 -0400

    modernized settings

--- settings.js
+++ settings.js
@@ -8,8 +8,6 @@
  *
  *  Copyright [2013-2016] [David P. Janes]
  *
- *  Much cleaner Settings than the one in IOTDB.js
- *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -28,7 +26,6 @@
 const _ = require('./helpers');
 
 const events = require('events');
-const util = require('util');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
@@ -44,20 +41,20 @@ const logger = _.logger.make({
  */
 const _paths = function() {
     const paths = [];
-    var current = process.cwd();
+    let current = process.cwd();
 
     while (true) {
-        var iotdb_folder = path.join(current, ".iotdb");
+        const iotdb_folder = path.join(current, ".iotdb");
 
         try {
-            var stbuf = fs.statSync(iotdb_folder);
+            const stbuf = fs.statSync(iotdb_folder);
             if (stbuf.isDirectory()) {
                 paths.push(iotdb_folder);
             }
         } catch (x) {
         }
 
-        var next = path.normalize(path.join(current, ".."));
+        const next = path.normalize(path.join(current, ".."));
         if (next === current) {
             break;
         }
@@ -68,137 +65,110 @@ const _paths = function() {
     return paths;
 };
 
-const Settings = function (paramd) {
-    const self = this;
+const make = (initd) => {
+    const self = Object.assign({}, events.EventEmitter.prototype);
 
-    self.paramd = _.defaults(paramd, {
+    const _initd = _.d.compose.shallow(initd, {
         root: "/",
         settings: "keystore.json",
-        path: _paths(), // [".iotdb", "$HOME/.iotdb", ],
-        makedirs: true,
+        path: _paths(), 
     })
     self.d = {}
 
-    events.EventEmitter.call(this);
-    this.setMaxListeners(0);
-
-    self._load();
-};
-
-util.inherits(Settings, events.EventEmitter);
-
-Settings.prototype._normalize_key = function (key) {
-    const self = this;
-
-    if (!key.match(/^\//)) {
-        key = self.paramd.root.replace('/*$', '') + '/' + key;
-    }
-
-    return "/" + key.replace(/^\/*/, '');
-};
-
-Settings.prototype._load = function () {
-    const self = this;
-    self.d = {};
-
-    const filenames = _.cfg.find(self.paramd.path, self.paramd.settings);
-    filenames.reverse();
-
-    const docds = [];
-    _.cfg.load.json(filenames, docd => docds.push(docd));
-
-    docds
-        .filter(docd => docd.error)
-        .forEach(docd => assert(!docd.error, docd.error));
-
-    docds
-        .forEach(docd => _.d.smart_extend(self.d, docd.doc));
-
-    self.emit("loaded");
-};
-
-/**
- */
-Settings.prototype.get = function (key, otherwise) {
-    const self = this;
-
-    key = self._normalize_key(key);
-
-    return _.d.get(self.d, key, otherwise);
-};
-
-/**
- */
-Settings.prototype.set = function (key, value) {
-    const self = this;
-
-    key = self._normalize_key(key);
-
-    _.d.set(self.d, key, value);
-
-    self.emit("changed", key);
-};
-
-/**
- */
-Settings.prototype.save = function (key, value, paramd) {
-    const self = this;
-
-    key = self._normalize_key(key);
-    paramd = _.defaults(paramd, {
-        global: false,
-        filename: null,
-        set: true,
-        mkdirs: false,
-    });
-
-    var filename;
-    if (paramd.filename) {
-        filename = paramd.filename
-    } else if (paramd.global) {
-        filename = self.paramd.path[self.paramd.path.length - 1] + "/" + self.paramd.settings;
-    } else {
-        filename = self.paramd.path[0] + "/" + self.paramd.settings;
-    }
-
-    // load settings
-    var d = {};
-    _.cfg.load.json([filename], function (paramd) {
-        for (var pd in paramd.doc) {
-            d[pd] = paramd.doc[pd];
+    const _normalize_key = function (key) {
+        if (!key.match(/^\//)) {
+            key = _initd.root.replace('/*$', '') + '/' + key;
         }
-    });
 
-    // if value is a function, we call it with the current value to get a new value
-    // this allows "in-place" updating of a particular value
-    if (_.is.Function(value)) {
-        value = value(_.d.get(d, key));
-    }
+        return "/" + key.replace(/^\/*/, '');
+    };
 
-    // update the (just loaded) settings
-    _.d.set(d, key, value);
+    const _load = function () {
+        self.d = {};
 
-    // save - XXX does not deal with recursion yet
-    if (paramd.mkdirs) {
-        var dirname = path.dirname(filename)
-        try {
-            fs.mkdirSync(dirname);
-        } catch (x) {}
-    }
-    fs.writeFileSync(filename, JSON.stringify(d, null, 2));
+        const filenames = _.cfg.find(_initd.path, _initd.settings);
+        filenames.reverse();
 
-    logger.info({
-        key: key,
-        value: value,
-        filename: filename,
-    }, "updated");
+        const docds = [];
+        _.cfg.load.json(filenames, docd => docds.push(docd));
 
-    // update ourselves
-    self.set(key, value);
-};
+        docds
+            .filter(docd => docd.error)
+            .forEach(docd => assert(!docd.error, docd.error));
 
-const make = (paramd) => {
-    return new Settings(paramd);
+        docds
+            .forEach(docd => _.d.smart_extend(self.d, docd.doc));
+
+        self.emit("loaded");
+    };
+
+    self.get = (key, otherwise) => _.d.get(self.d, _normalize_key(key), otherwise);
+
+    self.set = (key, value) => {
+        key = _normalize_key(key);
+
+        _.d.set(self.d, key, value);
+
+        self.emit("changed", key);
+    };
+
+    self.save = (key, value, paramd) => {
+        key = _normalize_key(key);
+        paramd = _.d.compose.shallow(paramd, {
+            global: false,
+            filename: null,
+            set: true,
+            mkdirs: false,
+        });
+
+        let filename;
+        if (paramd.filename) {
+            filename = paramd.filename
+        } else if (paramd.global) {
+            filename = _initd.path[_initd.path.length - 1] + "/" + _initd.settings;
+        } else {
+            filename = _initd.path[0] + "/" + _initd.settings;
+        }
+
+        // load settings
+        const d = {};
+        _.cfg.load.json([filename], function (paramd) {
+            for (const pd in paramd.doc) {
+                d[pd] = paramd.doc[pd];
+            }
+        });
+
+        // if value is a function, we call it with the current value to get a new value
+        // self allows "in-place" updating of a particular value
+        if (_.is.Function(value)) {
+            value = value(_.d.get(d, key));
+        }
+
+        // update the (just loaded) settings
+        _.d.set(d, key, value);
+
+        // save 
+        if (paramd.mkdirs) {
+            const dirname = path.dirname(filename)
+            try {
+                fs.mkdirSync(dirname);
+            } catch (x) {}
+        }
+        fs.writeFileSync(filename, JSON.stringify(d, null, 2));
+
+        logger.info({
+            key: key,
+            value: value,
+            filename: filename,
+        }, "updated");
+
+        self.set(key, value);
+    };
+
+    self.setMaxListeners(0);
+    _load()
+
+    return self;
 };
 
 let _settings;
